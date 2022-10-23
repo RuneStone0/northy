8f939ced72e1d567423322246b3a3a572ad391b5
Rune <rtk@rtk-cv.dk>
2022-10-23 22:32:03 +0000

    stuff

--- getpersonstweets.py
+++ getpersonstweets.py
@@ -15,21 +15,36 @@ db_collection = db["data2"]
 #authorize twitter, initialize tweepy
 auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
 auth.set_access_token(config["access_key"], config["access_secret"])
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# You can authenticate as your app with just your bearer token
+client = tweepy.Client(bearer_token=config["bearer_token"])
+
+# You can provide the consumer key and secret with the access token and access
+# token secret to authenticate as a user
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+
 
 screen_name_cache = {}
 def add_tweet_to_db(tweet):
+	"""
 	# Lookup account name from tweet and insert it into Mongo
 	# To avoid a million loopups, we create a simple cache
 	account_name = None
+	print(tweet.data)
+	print()
 	_author_id = tweet.user.id
 	if _author_id not in screen_name_cache.keys():
 		user = api.get_user(user_id=_author_id)
 		screen_name_cache[_author_id] = user.screen_name
-
+	"""
 	mydict = {
 		"tid": tweet.id_str,
-		"account_name": screen_name_cache[_author_id],
+		#"username": screen_name_cache[_author_id],
 		"created_at":  tweet.created_at,
 		"text": tweet.text 
 	}
@@ -87,9 +102,13 @@ def stream():
 				return False
 		
 		def on_tweet(self, tweet):
+			print(tweet.data)
+			print(tweet.includes)
 			tid = tweet.id
 			print(tid, tweet)
-			add_tweet_to_db(tweet)
+			#print(tweet.user_id)
+			#add_tweet_to_db(tweet)
+			sys.exit()
 
 	stream = listener(config["bearer_token"])
 	rule = "from:NTLiveStream OR from:52544B"
@@ -98,14 +117,58 @@ def stream():
 
 	# accounts with high tweeting frequency  ["everycolorbot", "tinycarebot", "dscovr_epic", "MagicRealismBot", "earthquakeBot", "MoMArobot", "poem_exe"]
 	print(f"+ Starting listener. Waiting for tweets..")
-	stream.filter() # , async=True
+	#stream.filter() # , async=True
 	
 	# For testing purposes.  Streams about 1% of all Tweets in real-time
-	#stream.sample()  
+	stream.sample()  
+
+
+def unit_testing():
+	#tweets = api.search_recent_tweets('Artificial Intelligence', count=1)
+	response = client.search_recent_tweets("AI", max_results=10, expansions=["author_id"], user_fields=['username'])
+	includes = response.includes
+	tweets = response.data
+	def _get_username(user_id):
+		for user in includes["users"]:
+			if user_id == user.id:
+				return user.username
+
+	for tweet in tweets:
+		print(tweet)
+		print(tweet.id, tweet.text)
+		print(tweet.author_id)
+		#print(tweet.username)
+		#add_tweet_to_db(tweets)
+		#username = _get_username(tweet.author_id)
+		#print(f"Username: {username}")
+
+
+def tweet_id_to_username(tweet_id=1578956182341185536):
+	tweet_ids = [tweet_id]
+
+	# By default, only the ID and text fields of each Tweet will be returned
+	# Additional fields can be retrieved using the tweet_fields parameter
+	response = client.get_tweets(tweet_ids, tweet_fields=["created_at"], expansions=["author_id"], user_fields=['username'])
+	includes = response.includes
+	for tweet in response:
+		#print(tweet)
+		print(includes)
+
+		username = includes["users"][0]
+		return username
+
+def unittest_tweet_id_to_username():
+	for tweet_id in [1578956182341185536,1579114131155623936,1578482250647502848]:
+		username = tweet_id_to_username(tweet_id)
+		print(username)
 
 if __name__ == '__main__':
-	create_db_index()
+	#create_db_index()
 	#pass in the username of the account you want to download
 	#get_all_tweets("NTLiveStream")
 	#get_all_tweets("52544B")
-	stream()
+	unittest_tweet_id_to_username()
+	tweet_id_to_username("1579114131155623936")
+	#stream()
+
+	#unit_testing()
