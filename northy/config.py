ad50e89a2f32317a643a33a252beac072e8fc120
Rune <rtk@rtk-cv.dk>
2023-06-19 21:35:11 -0500

    re-write twitter class + new cli

--- northy/config.py
+++ northy/config.py
@@ -1,17 +1,7 @@
-import ast
+import os
 from dotenv import dotenv_values
 
-class Config(object):
-    def __init__(self) -> None:
-        self.config = self.__get()
-
-    def __get(self):
-        """
-            Get config from .env file. If already fetched, return cached version.
-        """
-        self.config = dotenv_values(".env")
-
-        # Convert string to bool
-        self.config["PRODUCTION"] = ast.literal_eval(self.config["PRODUCTION"])
-
-        return self.config
+config = {
+    **dotenv_values(".env"),
+    **os.environ,  # override loaded values with environment variables
+}
