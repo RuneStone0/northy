80a983c65cdcd32600eb4d941b9a9cb83964f2e9
Rune <rtk@rtk-cv.dk>
2023-08-07 23:50:48 -0500

    convert PRODUCTION to bool

--- northy/config.py
+++ northy/config.py
@@ -5,3 +5,12 @@ config = {
     **dotenv_values(".env"),
     #**os.environ,  # override loaded values with environment variables
 }
+
+# Convert string representation of boolean to boolean
+production = config["PRODUCTION"]
+if production.lower() == "true":
+    config["PRODUCTION"] = True
+elif production.lower() == "false":
+    config["PRODUCTION"] = False
+else:
+    raise ValueError("Invalid string representation.")
