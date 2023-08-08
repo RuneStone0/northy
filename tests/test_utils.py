5ecd9e0a2399823c8e449a6c717b23a04beb03f9
Rune <rtk@rtk-cv.dk>
2023-08-07 22:11:00 -0500

    test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,34 +1,24 @@
 import os
 import sys
-from datetime import datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from northy import utils
+from northy.utils import Utils
 
-u = utils.Utils()
+u = Utils()
 
 def test_json_to_string():
     assert isinstance(u.json_to_string({"hello": "world"}), str)  # Is string
-
     # write more tests for utils.py
     
 def test_file_rw():
-    filename = "unittest.file"
+    filename = "tests/unittest.file"
     test_data = {"hello": "world"}
     assert u.write_json(test_data, filename=filename) == None
     assert u.read_json(filename=filename) == test_data
-    assert u.read_json(filename="invalid.file") == None
+    assert u.read_json(filename="nonexistingfile.txt") == None
 
-def test_get_config():
-    conf = u.get_config()
-    assert isinstance(conf, dict)
-    assert "mongodb_conn" in conf
+    # Cleanup
+    os.remove(filename)
 
-def test_serialize_datetime():
-    dt = datetime(2022, 4, 17, 10, 30, 45)
-    assert u.serialize_datetime(dt) == "2022-04-17T10:30:45"
-    assert isinstance(u.serialize_datetime(dt), str)
-
-def test_prowl():
-    assert u.prowl(message="test", priority=1) == None
-
-test_prowl()
+if __name__ == "__main__":
+    test_json_to_string()
+    test_file_rw()
