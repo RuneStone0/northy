0b6f88f3d521d443ab251b460f2554c7c89b5c7d
Rune <rtk@rtk-cv.dk>
2023-08-07 22:16:50 -0500

    switch logger

--- northy/logger.py
+++ northy/logger.py
@@ -1,49 +1,61 @@
-import os
 import logging
-from logging.handlers import RotatingFileHandler
-import coloredlogs
-from .config import config
 
-def get_logger(self, loggername="main", filename="logs.log"):
-    # get environment variable
-    LOG_LEVEL = config["LOG_LEVEL"]
-    root = logging.getLogger(loggername)
+class ColoredFormatter(logging.Formatter):
+    """
+        Color Options:
+            Black: \033[30m
+            Red: \033[31m
+            Green: \033[32m
+            Yellow: \033[33m
+            Blue: \033[34m
+            Magenta: \033[35m
+            Cyan: \033[36m
+            White: \033[37m
+    """
+    COLORS = {
+        'DEFAULT': '\033[0m',       # Reset to default color
+        'GREEN': '\033[32m',        # Green color for asctime
+        'YELLOW': '\033[33m',       # Yellow color for warning level
+        'RED': '\033[31m',          # Red color for error and critical levels
+    }
 
-    # Log to console
-    coloredlogs.install(
-        level=LOG_LEVEL,
-        logger=root,
-        fmt='%(asctime)s %(name)-11s %(funcName)-15s:%(lineno)-3s %(levelname)-8s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        level_styles={
-            # Colors: 'black', 'blue', 'cyan', 'green', 'magenta', 'red', 'white' and 'yellow'
-            'debug': {'color': 'white'},
-            'info': {'color': 'green'},
-            'warning': {'color': 'yellow'},
-            'error': {'color': 'red'},
-            'critical': {'color': 'magenta'},
-        },
-        field_styles={
-            'asctime': {'color': 'white'},
-            'name': {'color': 'white'},
-            'levelname': {'color': 'white'},
-            'message': {'color': 'blue'},
-        },
+    LEVEL_COLORS = {
+        'DEBUG': COLORS['DEFAULT'],        # Default color for DEBUG level
+        'INFO': '\033[36m',                 # Default color for INFO level
+        'WARNING': COLORS['YELLOW'],       # Yellow color for WARNING level
+        'ERROR': COLORS['RED'],            # Red color for ERROR level
+        'CRITICAL': COLORS['RED'],         # Red color for CRITICAL level
+    }
+
+    def format(self, record):
+        log_level = record.levelname
+        log_msg = super().format(record)
+
+        if log_level == 'WARNING':
+            log_msg = self.COLORS['YELLOW'] + log_msg + self.COLORS['DEFAULT']
+        elif log_level in ('ERROR', 'CRITICAL'):
+            log_msg = self.COLORS['RED'] + log_msg + self.COLORS['DEFAULT']
+
+        # Replace asctime with colored asctime
+        log_msg = log_msg.replace(record.asctime, self.COLORS['GREEN'] + record.asctime + self.COLORS['DEFAULT'])
+
+        # Apply custom color to levelname
+        log_msg = log_msg.replace(record.levelname, self.LEVEL_COLORS[log_level] + record.levelname + self.COLORS['DEFAULT'])
+
+        return log_msg
+
+def setup_logger():
+    # Configure the root logger
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        filename='northy.log',
+        filemode='a'
     )
 
-    # Log to file
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    logs_dir = os.path.join(dir_path, 'logs')
-    os.makedirs(logs_dir, exist_ok=True)
-    log_fname = os.path.join(logs_dir, filename)
-
-    fileHandler = RotatingFileHandler(log_fname, maxBytes=1000000, backupCount=10) #10 files of 1MB each
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fileHandler.setFormatter(formatter)
-
-    # Skip logging to file.
-    # When running `fetch --limit 200` we get anerror message indicating that a logging handler
-    # unable to rotate the log file because it was being used by another process
-    #root.addHandler(fileHandler)
-
-    return root
+    # Add a console handler for printing logs to the console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_formatter = ColoredFormatter('%(asctime)s %(levelname)s %(message)s')
+    console_handler.setFormatter(console_formatter)
+    logging.getLogger('').addHandler(console_handler)
