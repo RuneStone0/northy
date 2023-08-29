35b931c3d1de7d3705bd1624f1df2d705ca97de3
Rune <rtk@rtk-cv.dk>
2023-08-29 15:49:19 -0500

    added notifications to watcher

--- cli_signal.py
+++ cli_signal.py
@@ -2,6 +2,8 @@ import click
 import logging
 from northy.logger import setup_logger
 from northy.signal2 import Signal
+from northy.prowl import Prowl
+from northy.config import config
 
 if __name__ == '__main__':
     setup_logger()
@@ -21,9 +23,13 @@ if __name__ == '__main__':
 
     @click.command()
     def watch():
-        logger.info("watch")
-        signal = Signal()
-        signal.watch()
+        """ Watch for new tweets and signals """
+        try:
+            signal = Signal()
+            signal.watch()
+        except Exception as e:
+            p = Prowl(API_KEY=config["PROWL_API_KEY"])
+            p.send("Error: cli_signal.py crashed!!")
 
     cli.add_command(parse)
     cli.add_command(watch)
