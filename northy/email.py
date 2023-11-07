354729592c1758ad27d72576abb635c6fade8d59
Rune <rtk@rtk-cv.dk>
2023-11-07 16:28:13 -0600

    update generate closed position report

--- northy/email.py
+++ northy/email.py
@@ -1,24 +1,17 @@
-import os
-import logging
-from sparkpost import SparkPost
+from northy.config import config
+from postmarker.core import PostmarkClient
 
 class Email:
-    def __init__(self, API_KEY=None):
-        # Create a logger instance for the class
-        self.logger = logging.getLogger(__name__)
+    def __init__(self) -> None:
+        self.token = config["POSTMARK_SERVER_TOKEN"]
+        self.client = PostmarkClient(server_token=self.token)
 
-        # Get the API key from the environment variable
-        self.API_KEY = os.environ.get('SPARKPOST_API_KEY') if API_KEY is None else API_KEY
-
-    def send(self, to_email, subject, content):
-        sp = SparkPost(self.API_KEY)
-        response = sp.transmissions.send(
-            use_sandbox=False,
-            recipients=[to_email],
-            html=f'{content}',
-            from_email='northy@post.rtk-cv.dk',
-            subject=subject
+    def send(self, _from="rtk@rtk-cv.dk", to="rtk@rtk-cv.dk",
+             subject="Postmark test", content=None):
+        self.client.emails.send(
+            From=_from,
+            To=to,
+            Subject=subject,
+            #TextBody=content,
+            HtmlBody=content
         )
-        self.logger.info(response)
-
-
