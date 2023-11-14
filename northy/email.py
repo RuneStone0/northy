6372891b236d95de03750d90649abb4569f92e64
Rune <rtk@rtk-cv.dk>
2023-11-13 23:24:36 -0600

    fix pytests for email

--- northy/email.py
+++ northy/email.py
@@ -1,10 +1,10 @@
-from northy.config import config
+import os
 from postmarker.core import PostmarkClient
 
 class Email:
-    def __init__(self) -> None:
-        self.token = config["POSTMARK_SERVER_TOKEN"]
-        self.client = PostmarkClient(server_token=self.token)
+    def __init__(self, API_KEY=None) -> None:
+        self.API_KEY = os.environ.get('POSTMARK_SERVER_TOKEN') if API_KEY is None else API_KEY
+        self.client = PostmarkClient(server_token=self.API_KEY)
 
     def send(self, _from="rtk@rtk-cv.dk", to="rtk@rtk-cv.dk",
              subject="Postmark test", content=None):
--- tests/test_email.py
+++ tests/test_email.py
@@ -1,12 +1,12 @@
+import logging
 from northy.email import Email
 from northy.config import config, set_env
 
 def test_send():
-    # Pass API key as environment variable
-    set_env()
+    # Use API from environment variable
     email = Email()
-    email.send(to_email="test@rtk-cv.dk", subject="Test", content="Test")
+    email.send(subject="pytest", content="Test")
 
     # Pass API  key as argument
-    email = Email(API_KEY=config["SPARKPOST_API_KEY"])
-    email.send(to_email="test@rtk-cv.dk", subject="Test", content="Test")
+    email = Email(API_KEY=config["POSTMARK_SERVER_TOKEN"])
+    email.send(subject="pytest", content="Test")
