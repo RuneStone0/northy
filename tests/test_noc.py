190c2470cd792d727e0ecc5be6b18e195bf102e3
Rune <rtk@rtk-cv.dk>
2023-11-21 14:16:43 -0600

    handle tweets without text (image only)

--- tests/test_noc.py
+++ tests/test_noc.py
@@ -20,3 +20,14 @@ def test_process_notification():
         # Test against temp file
         noc = Noc(production=False, wpndatabase_path=temp_path)
         assert noc.process_notification() == None
+
+def test_notification_to_tweet():
+    # Test parsing notification without text (image posted)
+    # Example: https://twitter.com/elonmusk/status/1727011989661397193
+    toast = {'@launch': '0|0|Default|0|https://twitter.com/|p#https://twitter.com/#1tweet-1727011989661397193', '@displayTimestamp': '2023-11-21T17:13:21Z', 'visual': {'binding': {'@template': 'ToastGeneric', 'text': ['Elon Musk', None, {'@placement': 'attribution', '#text': 'twitter.com'}], 'image': {'@placement': 'appLogoOverride', '@src': 'C:\\Users\\rtk\\AppData\\Local\\Google\\Chrome\\User Data\\Notification Resources\\944d5e12-3a00-47a5-82fd-9f79f7f027ef.tmp', '@hint-crop': 'none'}}}, 'actions': {'action': {'@content': 'Go to Chrome notification settings', '@placement': 'contextMenu', '@activationType': 'foreground', '@arguments': '2|0|Default|0|https://twitter.com/|p#https://twitter.com/#1tweet-1727011989661397193'}}}
+    mock_notification = { "payload": { "toast": toast } }
+    noc = Noc(production=False, wpndatabase_path='tests/noc/wpndatabase.db')
+    noc.notification_to_tweet(mock_notification)
+    assert noc.notification_to_tweet(mock_notification)["from"] == 'Elon Musk'
+    assert noc.notification_to_tweet(mock_notification)["text"] == ''
+    
