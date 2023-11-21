7a3019a918e6eba87f944016610bca8efeced191
Rune <rtk@rtk-cv.dk>
2023-11-20 21:55:14 -0600

    improve error handling when parsing notifications

--- tests/test_noc.py
+++ tests/test_noc.py
@@ -6,12 +6,17 @@ def test_process_notification():
     # create /tmp if not exists
     if not os.path.exists('/tmp'): os.makedirs('/tmp')
 
-    filename = 'wpndatabase.db'
-    original_path = f'tests/noc/{filename}'
-    temp_path = f'/tmp/{filename}'
-    # copy wpndatabase.sqlite to /tmp
-    shutil.copy2(original_path, temp_path)
+    files = [
+        'wpndatabase.db',  # contains only tweets
+        'wpndatabase-surfacex.db',  # contains no tweets (important for testing)
+        'wpndatabase-windows10.db'
+    ]
+    for file in files:
+        original_path = f'tests/noc/{file}'
+        temp_path = f'/tmp/{file}'
+        # copy wpndatabase.sqlite to /tmp
+        shutil.copy2(original_path, temp_path)
 
-    # Test against temp file
-    noc = Noc(production=False, wpndatabase_path=temp_path)
-    assert noc.process_notification() == None
+        # Test against temp file
+        noc = Noc(production=False, wpndatabase_path=temp_path)
+        assert noc.process_notification() == None
