653fd3d41f2a2fc444e2908f82fbeeb5e58e1e34
Rune <rtk@rtk-cv.dk>
2023-11-21 23:08:02 -0600

    move saxo_config and optimize how its used

---
+++ conf/saxo_tickers.js
@@ -0,0 +1,26 @@
+[{
+    "NDX": {
+        "Uic": 4912, // US Tech 100 NAS (tracking NDX Index)
+        "AssetType": "CfdOnIndex",
+        "stoploss_points": 25,
+        "200ma": 11946
+    },
+    "SPX": {
+        "Uic": 4913, // US 500 (tracking SPX Index)
+        "AssetType": "CfdOnIndex",
+        "stoploss_points": 10,
+        "200ma": 3946
+    },
+    "DJIA": {
+        "Uic": 4911, // US 30 Wall Street (tracking DJIA Index)
+        "AssetType": "CfdOnIndex",
+        "stoploss_points": 25,
+        "200ma": 35215
+    },
+    "RUT": {
+        "Uic": 31933, // iShares Russell 2000 ETF (tracking RUT Index)
+        "AssetType": "CfdOnEtf",
+        "stoploss_points": 10,
+        "200ma": 170
+    }
+}]
