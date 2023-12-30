4fdf631de65ae45ecdc055d550d037e25dedb017
Rune <rtk@rtk-cv.dk>
2023-12-30 08:49:01 -0600

    add noc.ps1

---
+++ scripts/noc.ps1
@@ -0,0 +1,21 @@
+echo "cd into /northy"
+sleep 2
+cd C:\Users\User\Documents\code\RuneOrg\northy
+
+echo "Running git pull"
+sleep 2
+git pull
+
+echo "Activate virtual environment"
+sleep 2
+venv\Scripts\activate
+sleep 2
+
+echo "Install requirements.txt"
+sleep 2
+pip install -r .\requirements.txt
+
+echo "Start watching Windows Notification Center.."
+sleep 2
+.\cli_noc.py watch
+
