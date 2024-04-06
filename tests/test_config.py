df16a0502c22cd0cb56ccb0b3a7641fd9ae01509
Rune <rtk@rtk-cv.dk>
2024-04-06 09:41:55 -0500

    add test cases

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,20 @@
+import pytest
 from northy.config import Config
-config = Config().config
+c = Config()
 
 def test_config():
-    assert isinstance(config, dict)
-    config["PRODUCTION"]
+    assert isinstance(c.config, dict)
+    c.config["PRODUCTION"]
+
+def test_config_str2bool():
+    # Test False values
+    for i in ["false", "0", "no", "f"]:
+        assert c.str2bool(i) == False
+    
+    # Test True values
+    for i in ["true", "1", "yes", "t"]:
+        assert c.str2bool(i) == True
+
+def test_config_str2bool_invalid():
+    with pytest.raises(ValueError):
+        c.str2bool("invalid")
