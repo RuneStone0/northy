e7dcf2059759e42d50b7b433b8457f3f88c0bf4b
Rune <rtk@rtk-cv.dk>
2024-04-06 15:45:01 -0500

    skip sending emails when running test cases

--- northy/email.py
+++ northy/email.py
@@ -12,11 +12,19 @@ class Email:
         self.client = PostmarkClient(server_token=self.API_KEY)
 
     def send(self, _from="rtk@rtk-cv.dk", to="rtk@rtk-cv.dk",
-             subject="Postmark test", content=None):
+             subject="Postmark test", content=None) -> None:
+        
+        # If not in production, do not send the notification
+        msg = f"Email.send() called with:\nSubject: {subject}\nContent: {content}"
+        if config["PRODUCTION"] == False:
+            self.logger.debug(msg)
+            return
+    
+        # Send email
         self.client.emails.send(
             From=_from,
             To=to,
             Subject=subject,
             #TextBody=content,
             HtmlBody=content
-        )
+        ) # pragma: no cover
